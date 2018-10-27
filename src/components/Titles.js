e7adfacabf8b24a43ee20a25786fa86593347509
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-10-27 18:22:10 +0200

    Feat: Create and add <Titles/>

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Titles extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Get Weather</h1>
+                <p>Get temperature, expections and more...</p>
+            </div>
+        );
+    }
+};
+
+export default Titles;
