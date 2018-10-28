0886a59f1a649317557e2dd8b89f534043b0a6e2
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-10-28 05:23:46 +0200

    Feat: create and add <Form />

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Titles from './components/Titles';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Titles />
+        <Form />
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+    render() {
+        return(
+            <div>Form</div>
+        );
+    }
+}
+
+export default Form;
