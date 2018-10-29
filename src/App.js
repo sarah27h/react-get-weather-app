8f211abde6360bd47d9f5045f46aeb052fd754d0
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-10-29 21:21:58 +0200

    Feat: fetch weather data from openweathermap API
    Getting error Unhandled Rejection (SyntaxError):
    Unexpected token < in JSON at position 0
    
    this error means: unexpected token, <,
    is a strong clue that the response was HTML instead of JSON.
    
    this error caused due to typo in url forgetting -https://-

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+
+const API_KEY = '3b7d8b725e9415824bae9c91fae33399';
+
 class App extends Component {
+
+  componentDidMount() {
+
+    // use async await approche to fetch data from openweathermap API
+    // add async before our function
+    const getWeather = async () => {
+
+      // add await before make a call 
+      const api_call = await fetch(`api.openweathermap.org/data/2.5/forecast?q=London,uk&APPID=${API_KEY}`);
+
+      // handle response using json() method to turn response into JSON
+      const response = await api_call.json();
+      console.log(response);
+    }
+
+    getWeather();
+  }
+
+
   render() {
     return (
       <div className="App">
+      
         <Titles />
         <Form />
         <Weather />
