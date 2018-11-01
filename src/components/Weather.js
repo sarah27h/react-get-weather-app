10a514f82b07f4d6fff7104e46b203a0d0337288
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-01 18:05:13 +0200

    Feat: render data only if user click submit btn
    add conditional operator for rendering data
    only if user click submit btn

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 
 class Weather extends Component {
 
+    componentDidMount() {
+        console.log('Weather componentDidMount');        
+      }
+
     render() {
+
+        console.log('Weather render');
+
         return(
             <div>
-                <p>Location: {this.props.city}, {this.props.country}</p>
-                <p>Temperture: {this.props.temperture}</p>
-                <p>Humidity: {this.props.humidity}</p>
-                <p>Conditions: {this.props.condition}</p>
-                <p>Wind: Speed: {this.props.wind[0]}, Degree: {this.props.wind[1]}</p>
+                {/* add conditional operator for only rendering data if user click submit btn */}
+                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                { this.props.temperture && <p>Temperture: {this.props.temperture}</p> }
+                { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
+                { this.props.condition && <p>Conditions: {this.props.condition}</p> }
+                { this.props.wind[0] && this.props.wind[1] && <p>Wind: Speed: {this.props.wind[0]}, Degree: {this.props.wind[1]}</p> }
             </div>
         );
     }
