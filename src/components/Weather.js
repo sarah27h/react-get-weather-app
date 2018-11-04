9b56d15601fed6be9d6d70e31136d5fd9a957faf
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-05 00:11:36 +0200

    Style: add styles for <Weather />

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,13 +2,29 @@ import React from "react";
 
 function Weather(props) {
     return(
-        <div>
+        <div className="weather-wrapper">
             {/* add conditional operator for only rendering data if user click submit btn */}
-            { props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
-            { props.temperture && <p>Temperture: {props.temperture}</p> }
-            { props.humidity && <p>Humidity: {props.humidity}</p> }
-            { props.condition && <p>Conditions: {props.condition}</p> }
-            { props.wind[0] && props.wind[1] && <p>Wind: Speed: {props.wind[0]}, Degree: {props.wind[1]}</p> }
+            
+            { 
+                 
+            }
+
+            { 
+                props.city && props.country  && props.temperture && 
+                <div className="main-data">
+                    <p className="temp">{props.temperture}<sup className="degree">o</sup></p>
+                    <div className="vertical-line"></div>
+                    <p className="location"> {props.city}, {props.country}</p>
+                </div>
+
+            }
+
+            { props.humidity && <p className="weather-data"><span className="weather-title">Humidity:</span> {props.humidity}</p> }
+
+            { props.condition && <p className="weather-data"><span className="weather-title">Conditions:</span> {props.condition}</p> }
+
+            { props.wind[0] && props.wind[1] && <p className="weather-data"><span className="weather-title">Wind:</span> Speed: {props.wind[0]}, Degree: {props.wind[1]}</p> }
+            
             {props.error && <p>{props.error}</p>}
         </div>
     );
