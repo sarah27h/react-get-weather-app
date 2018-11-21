887bdebde2f68a043c477525ee408d1c3521da27
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-21 18:55:49 +0200

    Updates

---
+++ precache-manifest.a90fe98a160f4aba9b373f874c278d74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc4c0374704dd33b0d73f2499fadf5f0",
+    "url": "/react-get-weather-app/static/media/app_logo.bc4c0374.png"
+  },
+  {
+    "revision": "fb1a151ed759a03dba9c",
+    "url": "/react-get-weather-app/static/js/runtime~main.fb1a151e.js"
+  },
+  {
+    "revision": "3666b04f64cdb9d99cc9",
+    "url": "/react-get-weather-app/static/js/main.3666b04f.chunk.js"
+  },
+  {
+    "revision": "7b6c2ae0059c41c95d85",
+    "url": "/react-get-weather-app/static/js/1.7b6c2ae0.chunk.js"
+  },
+  {
+    "revision": "3666b04f64cdb9d99cc9",
+    "url": "/react-get-weather-app/static/css/main.013221e6.chunk.css"
+  },
+  {
+    "revision": "a81c6d9fb28df7864e8db3acde990691",
+    "url": "/react-get-weather-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-get-weather-app/precache-manifest.a90fe98a160f4aba9b373f874c278d74.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-get-weather-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
